94e561a4f7850bc3c260c1c5425f32a6345b5fe7
MAHAFUJUR RAHMAN MUNNA <mahafujurrahman4480@gmail.com>
2026-06-23 09:29:45 +0600

    create user collection

--- index.js
+++ index.js
@@ -26,68 +26,103 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-  const ArtHubDB = client.db('ArtHub');
-  const artWorksCollection = ArtHubDB.collection('artworks');
-  const paymentsCollection = ArtHubDB.collection('payments');
-  const plansCollection = ArtHubDB.collection('plans');
+    const ArtHubDB = client.db('ArtHub');
+    const artWorksCollection = ArtHubDB.collection('artworks');
+    const paymentsCollection = ArtHubDB.collection('payments');
+    const plansCollection = ArtHubDB.collection('plans');
+    const purchasesCollection = ArtHubDB.collection('purchases');
+    const userCollection = ArtHubDB.collection('user');
 
 
-  // artist api
-  app.post('/api/artists', async (req, res) => {
-   const data = req.body;
-   const newArtwork = {
-    ...data,
-    createAt: new Date(),
-   }
-   const result = await artWorksCollection.insertOne(newArtwork);
-   res.json(result);
+    // artist api
+    app.post('/api/artists', async (req, res) => {
+      const data = req.body;
+      const newArtwork = {
+        ...data,
+        createAt: new Date(),
+      }
+      const result = await artWorksCollection.insertOne(newArtwork);
+      res.json(result);
+    });
+    // delete artwork
+    app.delete('/api/artwork/:id', async (req, res) => {
+      const { id } = req.params;
+      const result = await artWorksCollection.deleteOne({ _id: new ObjectId(id) });
+      res.json(result);
+    });
+    app.patch('/api/artwork/:id', async (req, res) => {
+      const { id } = req.params;
+      const data = req.body;
+      const result = await artWorksCollection.updateOne({ _id: new ObjectId(id) }, { $set: data });
+      res.json(result);
+    });
+    // get artworks
+    app.get('/api/artwork', async (req, res) => {
+      const result = await artWorksCollection.find().toArray();
+      res.json(result);
+    });
+    //get artwork by id
+    app.get('/api/artwork/:id', async (req, res) => {
+      const { id } = req.params;
+      const result = await artWorksCollection.findOne({ _id: new ObjectId(id) });
+      res.json(result);
+    });
+   app.get('/api/purchases', async (req, res) => {
+     const userId = req.query.userId
+      const result = await purchasesCollection.find({_id: new ObjectId(userId)}).toArray();
+      res.json(result);
+    });
+    // PAYMENTS RELATED API
+    app.post('/api/payments', async (req, res) => {
+      const data = req.body;
+      const { sessionId, type, amount, artistName, artworkName, artworkId, image, artistId, buyerId, buyerName } = data;
+
+      const existingPayment = await paymentsCollection.findOne({ sessionId });
+      if (existingPayment) {
+        return
+      }
+      // purchases
+      const purchaseObj = {
+        image,
+        artworkName,
+        artworkId,
+        buyerId,
+      }
+      await purchasesCollection.insertOne(purchaseObj);
+
+      // history 
+      const newPayment = {
+        artistId,
+        artistName,
+        artworkName,
+        buyerId,
+        buyerName,
+        amount,
+        sessionId,
+        createAt: new Date(),
+      }
+      await paymentsCollection.insertOne(newPayment);
+    });
+
+  // subscriptions
+  app.post('/api/subscriptions', async (req, res) =>{
+      const data = req.body;
+      const userId = data.buyerId;
+      const user = await 
+      
+      const newSubscription = {
+        ...data,
+        createAt: new Date(),
+      }
+      await paymentsCollection.insertOne(newSubscription);
   });
-// delete artwork
-app.delete('/api/artwork/:id', async (req, res) => {
-    const { id } = req.params;
-    const result = await artWorksCollection.deleteOne({ _id: new ObjectId(id) });
-    res.json(result);
-});
-app.patch('/api/artwork/:id', async (req, res) => {
-    const { id } = req.params;
-    const data = req.body;
-    const result = await artWorksCollection.updateOne({ _id: new ObjectId(id) }, { $set: data });
-    res.json(result);
-});
-// get artworks
-app.get('/api/artwork', async(req, res)=>{
-    const result = await artWorksCollection.find().toArray();
-    res.json(result);
-});
-//get artwork by id
-app.get('/api/artwork/:id', async(req, res)=>{
-  const { id } = req.params;
-  const result = await artWorksCollection.findOne({ _id: new ObjectId(id) });
-  res.json(result);
-});
 
-// PAYMENTS RELATED API
-app.post('/api/payments', async (req, res) => {
-    const data = req.body;
-    const sessionId = data.sessionId;
-    const existingPayment = await paymentsCollection.findOne({ sessionId });
-    if(existingPayment){
-      return
-    }
-    
-    const newPayment = {
-      ...data,
-      createAt: new Date(),
-    }
-    const result = await paymentsCollection.insertOne(newPayment);
-    res.json(result);
-  });
-
-//PLANS RELATED API
-app.get('/api/plans', async (req, res) => {
-    const result = await plansCollection.find().toArray();
-    res.json(result);
-  });
+    //PLANS RELATED API
+    app.get('/api/plans', async (req, res) => {
+      const plan = req.query.plan;
+      const result = await plansCollection.findOne({ plan });
+      res.json(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -103,8 +138,8 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+  res.send('Hello World');
 });
-app.listen(process.env.PORT , () => {
-    console.log('Server is running on port 5000');
+app.listen(process.env.PORT, () => {
+  console.log('Server is running on port 5000');
 });
